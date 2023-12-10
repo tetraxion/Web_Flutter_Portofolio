9b8c36d802c630ae993da1d4effb3e635d06bdda
Tetraxion <lutfid955@gmail.com>
2023-12-10 23:05:44 +0700

    fist commit

--- lib/models/project.dart
+++ lib/models/project.dart
@@ -13,15 +13,69 @@ List<Project> demo_projects = [
         "- Responsive\n- Launch URL\n- Clean Code and a lot of more...",
   ),
   Project(
-    title: "",
-    link: "",
+    title: "Flutter Bidan Delima",
+    link: "https://github.com/tetraxion/bidan_delima.git",
     description:
-        "- Login with google\n- Login with google\n- After Login, show details ( profile picture, name, email ) and a lot of more...",
+        "- Login Firebase\n- After Login, show details ( profile picture, name, email ) and a lot of more...",
   ),
   Project(
-    title: "",
-    link: "",
+    title: "Flutter Coffe Shop",
+    link: "https://github.com/tetraxion/Flutter_CoffeShop.git",
     description:
         "- Responsive Apps\n- Buy Product\n- Add Product to Shopping cart\n- Search Delegate and a lot of more...",
+  ),
+  Project(
+    title: "Flutter Slicing Dana",
+    link: "https://github.com/tetraxion/slicing_dana.git",
+    description:
+        "- Responsive Apps\n- Buy Product\n- Search Delegate and a lot of more...",
+  ),
+  Project(
+    title: "Flutter Example Login",
+    link: "https://github.com/tetraxion/example_login_flutter_unguided.git",
+    description:
+        "- Responsive Apps\n- Login\n- After Login\n- Search Delegate and a lot of more...",
+  ),
+  Project(
+    title: "Laravel Booking Gwon MarieLocation",
+    link: "https://github.com/tetraxion/MarieLocation.git",
+    description:
+        "- Responsive Webs\n- Buy Product\n- Admin management (Add, Edit, Delete)\n- Search Delegate and a lot of more...",
+  ),
+  Project(
+    title: "Laravel Master Data Storage",
+    link: "https://github.com/tetraxion/miniProjectUTS.git",
+    description:
+        "- Responsive Webs\n- Add Product\n- Edit Product\n- Delete Product Search Delegate and a lot of more...",
+  ),
+  Project(
+    title: "Bootstrap Clone",
+    link: "https://github.com/tetraxion/bootstrap-clone.git",
+    description:
+        "- Responsive Webs\n- Search Delegate and a lot of more...",
+  ),
+  Project(
+    title: "Php KAI Clone",
+    link: "https://github.com/tetraxion/KAI_PHP.git",
+    description:
+        "- Responsive Webs\n- Buy Product\n- Admin management (Add, Edit, Delete)\n- Search Delegate and a lot of more...",
+  ),
+  Project(
+    title: "HTML Ecommerce NuShoptara",
+    link: "https://github.com/tetraxion/Nushoptara_html.git",
+    description:
+        "- Landing pages webs\n-  Search Delegate and a lot of more...",
+  ),
+  Project(
+    title: "HTML Ticket BlackPink",
+    link: "https://github.com/tetraxion/Html_TicketBlackpink.git",
+    description:
+        "- Landing pages webs\n- Search Delegate and a lot of more...",
+  ),
+  Project(
+    title: "Python Apotik",
+    link: "https://github.com/tetraxion/Apotik_GUI_Python.git",
+    description:
+        "- Responsive Webs\n- Admin management (Add, Edit, Delete)\n- Add Product \n- Search Delegate and a lot of more...",
   ),
 ];
--- lib/screen/main/component/side_menu.dart
+++ lib/screen/main/component/side_menu.dart
@@ -4,6 +4,7 @@ import 'package:portofolio/screen/main/component/download_cv.dart';
 import 'package:portofolio/screen/main/component/skill_menu.dart';
 import 'package:portofolio/screen/main/component/social_media.dart';
 
+
 class SideMenu extends StatelessWidget {
   const SideMenu({
     Key? key,
@@ -24,33 +25,33 @@ class SideMenu extends StatelessWidget {
             height: defaultPadding,
           ),
           SkillMenu(
-            image1: 'images/Dart.png',
+            image1: 'assets/images/Dart.png',
             text1: 'Dart',
-            image2: 'images/flutter.png',
+            image2: 'assets/images/flutter.png',
             text2: 'Flutter',
-            image3: 'images/python.png',
+            image3: 'assets/images/python.png',
             text3: 'Python',
           ),
           SizedBox(
             height: defaultPadding,
           ),
           SkillMenu(
-            image1: 'images/htmlcssjs.png',
+            image1: 'assets/images/htmlcssjs.png',
             text1: 'HTML, CSS, JS',
-            image2: 'images/php.png',
+            image2: 'assets/images/php.png',
             text2: 'PHP',
-            image3: 'images/laravel.png',
+            image3: 'assets/images/laravel.png',
             text3: 'Laravel',
           ),
           SizedBox(
             height: defaultPadding,
           ),
           SkillMenu(
-            image1: 'images/mysql.png',
+            image1: 'assets/images/mysql.png',
             text1: 'Mysql',
-            image2: 'images/firebase.png',
+            image2: 'assets/images/firebase.png',
             text2: 'Firebase',
-            image3: 'images/git.png',
+            image3: 'assets/images/git.png',
             text3: 'Git',
           ),
           SizedBox(height: defaultPadding),
